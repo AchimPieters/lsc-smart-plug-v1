2070bdbeaeff69656327a56d7be87b59b8dc0e0f
Achim Pieters <Achim.Pieters@outlook.com>
2023-04-17 18:15:29 +0200

    Update software_versioning.h

--- scr/software_versioning.h
+++ scr/software_versioning.h
@@ -1,5 +1,5 @@
 /*
-   Copyright 2019 Achim Pieters | StudioPieters®
+   Copyright 2023 Achim Pieters | StudioPieters®
 
    Permission is hereby granted, free of charge, to any person obtaining a copy
    of this software and associated documentation files (the "Software"), to deal
